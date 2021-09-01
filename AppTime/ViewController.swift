e5c6c2a51df75b23a6b30654280756f0bcce0df5
phillt3 <tracypb@bc.edu>
2021-09-01 10:48:43 -0400

    2.8 added ifs

--- AppTime/ViewController.swift
+++ AppTime/ViewController.swift
@@ -16,10 +16,19 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     @IBAction func firstButtonPressed(_ sender: Any) {
-        riseLabel.text = "RISE"
-        blueEarthIMG.image = UIImage(named: "rise_earth")
+        let msg1 = "RISE"
+        let msg2 = "The RISE"
+        let msg3 = "RISE UP"
+        if riseLabel.text == msg1 {
+            riseLabel.text = msg2
+            blueEarthIMG.image = UIImage(named: "rise_earth")
+        } else if riseLabel.text == msg2 {
+            riseLabel.text = msg3
+            blueEarthIMG.image = UIImage(named: "blue_sun")
+        } else {
+            riseLabel.text = msg1
+            blueEarthIMG.image = UIImage(named: "cartoon_sun")
+        }
     }
-    
-
 }
 
