45e4c5c23c262dfeb16a5f91640b3b3c494e6bb3
phillt3 <tracypb@bc.edu>
2021-08-31 21:10:32 -0400

    2.7 First AppTime commit

--- AppTime/ViewController.swift
+++ AppTime/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var riseLabel: UILabel!
+    @IBOutlet weak var blueEarthIMG: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func firstButtonPressed(_ sender: Any) {
+        riseLabel.text = "RISE"
+        blueEarthIMG.image = UIImage(named: "rise_earth")
+    }
+    
 
 }
 
