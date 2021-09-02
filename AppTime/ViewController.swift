52c8e1f21fb7dc9959f2849f745c972e4d59068d
phillt3 <tracypb@bc.edu>
2021-09-02 16:28:48 -0400

    2.9 Multiple Images Added

--- AppTime/ViewController.swift
+++ AppTime/ViewController.swift
@@ -11,24 +11,34 @@ class ViewController: UIViewController {
     @IBOutlet weak var riseLabel: UILabel!
     @IBOutlet weak var blueEarthIMG: UIImageView!
     
+    var imageNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     @IBAction func firstButtonPressed(_ sender: Any) {
-        let msg1 = "RISE"
-        let msg2 = "The RISE"
-        let msg3 = "RISE UP"
-        if riseLabel.text == msg1 {
-            riseLabel.text = msg2
-            blueEarthIMG.image = UIImage(named: "rise_earth")
-        } else if riseLabel.text == msg2 {
-            riseLabel.text = msg3
-            blueEarthIMG.image = UIImage(named: "blue_sun")
-        } else {
-            riseLabel.text = msg1
-            blueEarthIMG.image = UIImage(named: "cartoon_sun")
+//        let imageName = "image" + String(imageNumber)
+        let imageName = "image\(imageNumber)"
+        blueEarthIMG.image = UIImage(named: imageName)
+        imageNumber = imageNumber + 1
+        if imageNumber == 6 {
+            imageNumber = 0
         }
+
+        //        let msg1 = "RISE"
+        //        let msg2 = "The RISE"
+        //        let msg3 = "RISE UP"
+        //        if riseLabel.text == msg1 {
+        //            riseLabel.text = msg2
+        //            blueEarthIMG.image = UIImage(named: "image0")
+        //        } else if riseLabel.text == msg2 {
+        //            riseLabel.text = msg3
+        //            blueEarthIMG.image = UIImage(named: "image1")
+        //        } else {
+        //            riseLabel.text = msg1
+        //            blueEarthIMG.image = UIImage(named: "image2")
+        //        }
     }
 }
 
