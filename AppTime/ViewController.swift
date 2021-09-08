e21783cefa3f1e966f88d8598bf568e7b957ea21
phillt3 <tracypb@bc.edu>
2021-09-08 10:52:22 -0400

    2.13 Loops and Repeats

--- AppTime/ViewController.swift
+++ AppTime/ViewController.swift
@@ -11,32 +11,33 @@ class ViewController: UIViewController {
     @IBOutlet weak var riseLabel: UILabel!
     @IBOutlet weak var blueEarthIMG: UIImageView!
     
-    var messages = ["RiseDigital", "risedigital.org", "rise.com","rise.net", "RISE", "The Rise", "RISE UP"]
+    var imageNumber = -1
+    var messageNumber = -1
+    var totalNumberOfImages = 5
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     @IBAction func firstButtonPressed(_ sender: Any) {
-//        let imageName = "image" + String(imageNumber)
-        blueEarthIMG.image = UIImage(named: "image\(Int.random(in: 1...5))")
-        riseLabel.text = messages[Int.random(in: 0...messages.count-1)]
-
+        let messages = ["RiseDigital", "risedigital.org",
+                        "rise.com", "rise.net", "RISE",
+                        "The Rise", "RISE UP"]
+        var newMessageNumber: Int
+        repeat {
+            newMessageNumber = Int.random(in: 0...messages.count-1)
+        } while messageNumber == newMessageNumber
+        messageNumber = newMessageNumber
+        riseLabel.text = messages[messageNumber]
         
-
-        //        let msg1 = "RISE"
-        //        let msg2 = "The RISE"
-        //        let msg3 = "RISE UP"
-        //        if riseLabel.text == msg1 {
-        //            riseLabel.text = msg2
-        //            blueEarthIMG.image = UIImage(named: "image0")
-        //        } else if riseLabel.text == msg2 {
-        //            riseLabel.text = msg3
-        //            blueEarthIMG.image = UIImage(named: "image1")
-        //        } else {
-        //            riseLabel.text = msg1
-        //            blueEarthIMG.image = UIImage(named: "image2")
-        //        }
+        var newImageNumber: Int
+        repeat {
+            newImageNumber = Int.random(in: 0 ... totalNumberOfImages)
+        } while imageNumber == newImageNumber
+        imageNumber = newImageNumber
+        blueEarthIMG.image = UIImage(named: "image\(imageNumber)")
+        
     }
 }
 
